8f12656c00d4c25043b3cf796153a72d3c27fd00
uberextfiles-byte <uber.ext.files@gmail.com>
2026-02-03 14:34:00 +0530

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Voice Gig App</h1>
-      <p>App migration in progress...</p>
-    </div>
-  )
-}
-// redeploy fix
